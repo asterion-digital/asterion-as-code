b0944c26522b085beafd6c77d25652c9e9de1f96
James Blair <mail@jamesblair.net>
2022-03-12 11:54:57 +1300

    Add local chart for generic mariadb.

--- app-wordpress/__main__.py
+++ app-wordpress/__main__.py
@@ -8,33 +8,11 @@ from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
 from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs
 from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs, RepositoryOptsArgs, Chart, LocalChartOpts
 
-# Generate mariadb password
-#mariadb_root = random.RandomPassword("mariadb-root-password", length=12)
-#mariadb_pass = random.RandomPassword("mariadb-password", length=12)
-
-# Deploy the wordpress standalone mariadb
-mariadb = Release(
+mariadb = Chart(
     "wpdev-mariadb",
-    ReleaseArgs(
-        chart="mariadb",
-        repository_opts=RepositoryOptsArgs(
-            repo="https://charts.bitnami.com/bitnami",
-        ),
-        # Override chart values.
-        version="10.3.7",
-        values={
-            "image": {
-                "registry": "ghcr.io",
-                "repository": "zcube/bitnami-compat/mariadb",
-                "tag": "10.6"
-            },
-            "auth" : {
-                "database": "wordpress",
-                "username": "wordpress",
-                "password": "wordpress"
-            }
-        },
-    ),
+    LocalChartOpts(
+        path="./charts/mariadb"
+    )
 )
 
 wordpress = Chart(
@@ -43,6 +21,3 @@ wordpress = Chart(
         path="./charts/wordpress"
     ),
 )
-
-# Export application information
-pulumi.export("Database name", mariadb.name)
