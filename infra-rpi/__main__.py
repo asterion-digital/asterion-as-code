b51c260754fc59f0cd0dc306ff657801d68a842d
shawn@mandalore <shawn.gerrard@gmail.com>
2022-03-22 12:26:15 +1300

    Removed persistent volume and storage class definitions in main.py

--- infra-rpi/__main__.py
+++ infra-rpi/__main__.py
@@ -3,6 +3,8 @@
 import pulumi
 import pulumi_command as command
 from pulumi import Output
+from pulumi_kubernetes.storage.v1 import StorageClass
+
 
 # Get pulumi config
 config = pulumi.Config()
@@ -11,6 +13,51 @@ config = pulumi.Config()
 public_key = config.require('publickey')
 private_key = config.require_secret('privatekey')
 ip_address = config.require('ip_address')
+
+###########################################################
+
+# Define a storage class for persistent volumes on the cluster
+# TO DO 1: transfer code to infra codebase
+sc = StorageClass(
+    "asterion-dev-sc-localfs1",
+    provisioner="kubernetes.io/no-provisioner",
+    volume_binding_mode="WaitForFirstConsumer"
+)
+
+# Define a persistent volume for the cluster
+# TO DO 1: specify dependence on SC?
+# TO DO 2: transfer code to infra codebase
+pv = PersistentVolume(
+    "asterion-dev-pv-localfs1",
+    metadata={},
+    spec={
+        "access_modes": [ "ReadWriteOnce" ],
+        "capacity": {
+            "storage": "100Gi"
+        },
+        "local": {
+            "path": "/mnt/data/k3s",
+            "fs_type": "ext4"
+        },
+        "node_affinity": {
+            "required": {
+                "node_selector_terms": [ {
+                    "match_expressions": [ {
+                        "key": "kubernetes.io/hostname",
+                        "operator": "In",
+                        "values": [ "litrepublicpi" ]
+                    } ]
+                } ]
+            }
+        },
+        "storage_class_name": "asterion-dev-sc-localfs1",
+        "volume_mode": "Filesystem"
+    }
+)
+
+###########################################################
+
+
 
 # Define a remote provider connection
 connection = command.remote.ConnectionArgs(
