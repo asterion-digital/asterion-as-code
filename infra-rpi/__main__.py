16a3d9ebaf612fff642a3f6c2fb78cf97664d849
shawn@Ultron <shawn.gerrard@gmail.com>
2022-03-28 18:08:12 +1300

    Added code to retrieve kubeconfig from master node and store locally

--- infra-rpi/__main__.py
+++ infra-rpi/__main__.py
@@ -21,31 +21,38 @@ connection = command.remote.ConnectionArgs(
     )
 
 # Declare command to install k3s on the remote node
-install_cmd = Output.concat(
+installk3s_cmd = Output.concat(
     "curl -sfL https://get.k3s.io | sh -s - server --tls-san ", server_ip_address, " --write-kubeconfig-mode 644 --no-deploy traefik --no-deploy servicelb --data-dir /mnt/data/k3s"
     )
 
-# Declare command to uninstall k3s on the remote node
-uninstall_cmd = Output.concat(
-    "/usr/local/bin/k3s-uninstall.sh"
-    )
-
-# Send the k3s installation command to the remote note
+# Register k3s install on create and uninstall on delete with the remote node
 install_k3s = command.remote.Command(
     'asterion-infra-rpi-k3s-installer',
     connection=connection,
-    create=install_cmd.apply(lambda v: v),
-    delete=uninstall_cmd.apply(lambda v: v)
+    create=installk3s_cmd.apply(lambda v: v),
+    delete="/usr/local/bin/k3s-uninstall.sh"
 )
 
-# Declare command to retrieve kubeconfig from the remote node
-retrieve_kubeconfig = command.remote.Command(
-    'asterion-infra-retrieve-kubeconfig',
+# Implement retrieval of kubeconfig from the remote node
+output_kubeconfig = command.remote.Command(
+    'asterion-infra-rpi-retrieve-kubeconfig',
     connection=connection,
     create='cat /etc/rancher/k3s/k3s.yaml',
     opts=pulumi.ResourceOptions(depends_on=[install_k3s])
 )
 
+# Declare command to set kubeconfig file on the local environment
+setkubeconfig_cmd = Output.concat(
+    "echo '", output_kubeconfig.stdout, "' > ~/.kube/config && sed -i 's/127.0.0.1/", server_ip_address, "/g' ~/.kube/config"
+    )
+
+# Set local kubernetes configuration file
+set_local_kubeconfig = command.local.Command(
+    'asterion-infra-rpi-set-local-kubeconfig',
+    create=setkubeconfig_cmd.apply(lambda v: v),
+    opts=pulumi.ResourceOptions(depends_on=[output_kubeconfig])
+)
+
 # Export output to the terminal window
-pulumi.export('Infra server kubeconfig', retrieve_kubeconfig.stdout)
+pulumi.export('Infra server kubeconfig', output_kubeconfig.stdout)
 pulumi.export('Infra server public ip', server_ip_address)
