90fb9da3404166a80839d81e732d86ad992ed879
Shawn Gerrard <shawn.gerrard@gmail.com>
2022-05-01 13:45:28 +1200

    Cleaned up infra-aws file directory

--- org-asterion/__main__.py
+++ org-asterion/__main__.py
@@ -7,14 +7,14 @@ import pulumi_aws as aws
 class org:
 
     # Default constructor
-    def __init__(self, name, org):
+    def __init__(self, name):
         self.name = name
-        self.org = org
+        self.org = aws.organizations.get_organization()
 
-    # Create an aws org for the current account
+    # Create an aws organization for the current account
     def create_org(self):
         
-        # Create an organization for this current account
+        # Create an aws organization for this current account
         self.org = aws.organizations.Organization(self.name,
             aws_service_access_principals=[
                 "cloudtrail.amazonaws.com",
@@ -22,19 +22,17 @@ class org:
             ],
             feature_set="ALL")
 
-    # Check if an aws org exists for this account
+    # Check if an aws organization exists for this account
     def org_exists(self):
         if self.org.id == "" or self.org.id is none:
             return False
         else:
             return True
 
-# Create an aws org object for asterion-infra-aws
-organization = org(
-    'asterion-infra-aws',
-    aws.organizations.get_organization())
+# Create an aws object for the asterion-infra-aws organization
+asterion_infra_aws_org = org('asterion-infra-aws')
 
-# Check if the org object is set else set it
-if not organization.org_exists:
-    organization.create_org()
-pulumi.export("Asterion aws org ID", organization.org.id)
+# Check if the asterion aws organization object is set else set it
+if not asterion_infra_aws_org.org_exists:
+    asterion_infra_aws_org.create_org()
+pulumi.export("Asterion aws org ID", asterion_infra_aws_org.org.id)
