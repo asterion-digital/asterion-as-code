aeb3fa78275887d2a2e25a4bda313b95cc08a45a
Shawn Gerrard <shawn.gerrard@gmail.com>
2022-05-22 13:29:23 +1200

    Added exception handling to create call in org class of main.py

--- org-asterion/__main__.py
+++ org-asterion/__main__.py
@@ -17,29 +17,39 @@ class org:
 
     # Static method to create an aws organization
     def create_org(self):
+        
+        # Attempt to create an aws organization for this current account
+        try:
+            self.org = aws.organizations.Organization(
+                self.name,
+                aws_service_access_principals=[
+                    "cloudtrail.amazonaws.com",
+                    "config.amazonaws.com",
+                    "account.amazonaws.com",
+                ],
+                feature_set="ALL")
 
-        # Create an aws organization for this current account
-        self.org = aws.organizations.Organization(
-            self.name,
-            aws_service_access_principals=[
-                "cloudtrail.amazonaws.com",
-                "config.amazonaws.com",
-                "account.amazonaws.com",
-            ],
-            feature_set="ALL")
+            # Set the root id for the aws organization
+            self.rootid = self.org.roots[0].id
 
-        # Set the root id for the aws organization
-        self.rootid = self.org.roots[0].id
+        except BaseException as err:
+            pulumi.log.info("PYLOGGER (" + str(datetime.datetime.now()) + "): There was a critical exception found in the 'create_org()' method of the 'org' class")
+            pulumi.log.info("PYLOGGER (" + str(datetime.datetime.now()) + "): " + str(err))
 
-    # Check if an aws organization exists for this account
+    # Static method to check if an aws organization exists for this account
     def org_exists(self):
+
+        # Attempt to obtain the organization
         try:
             self.org = aws.organizations.get_organisation()
             self.rootid = self.org.roots[0].id
+
+            # Check if the organization has any root accounts
             if self.org.roots[0] is None:
                 return False
             else:
                 return True
+
         except BaseException as err:
             pulumi.log.info("PYLOGGER (" + str(datetime.datetime.now()) + "): There was a critical exception found in the 'org_exists()' method of the 'org' class")
             pulumi.log.info("PYLOGGER (" + str(datetime.datetime.now()) + "): " + str(err))
